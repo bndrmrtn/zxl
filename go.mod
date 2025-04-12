ea48ca73f1a0aba4e476f2032b03e0c7feffaa5b
Martin Binder <bndrmrtn@gmail.com>
2025-04-12 15:35:08 +0200

    testing added

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/manifoldco/promptui v0.9.0
 	github.com/spf13/cobra v1.9.1
+	github.com/stretchr/testify v1.10.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.35.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -22,6 +23,7 @@ require (
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/cloudflare/circl v1.6.0 // indirect
 	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.6.2 // indirect
@@ -31,6 +33,7 @@ require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pjbgf/sha1cd v0.3.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/skeema/knownhosts v1.3.1 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
--- internal/ast/ast_test.go
+++ internal/ast/ast_test.go
@@ -1,213 +1,86 @@
 package ast
 
 import (
-	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/bndrmrtn/zxl/internal/lexer"
 	"github.com/bndrmrtn/zxl/internal/models"
 	"github.com/bndrmrtn/zxl/internal/tokens"
+	"github.com/stretchr/testify/assert"
 )
 
+func build(t *testing.T, s string) []*models.Node {
+	ts, err := lexer.New("<test>").Parse(strings.NewReader(s))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := NewBuilder()
+	nodes, err := b.Build(ts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return nodes
+}
+
 func Test_AstVarDelc(t *testing.T) {
-	ts := []*models.Token{
-		{
-			Type:  tokens.Let,
-			Value: "let",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "a",
-		},
-		{
-			Type:  tokens.Assign,
-			Value: "=",
-		},
-		{
-			Type:  tokens.Number,
-			Value: "25",
-		},
-		{
-			Type:  tokens.Semicolon,
-			Value: ";",
-		},
-	}
+	nodes := build(t, "let x = 5;")
 
-	b := NewBuilder()
-	nodes, err := b.Build(ts)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Equal(t, 1, len(nodes), "must create on variable node")
+	variable := nodes[0]
 
-	for _, node := range nodes {
-		fmt.Println(node.String())
-	}
+	assert.Equal(t, "x", variable.Content, "variable name must be x")
+	assert.Equal(t, 5, variable.Value, "variable value must be int 5")
 }
 
-func Test_AstFuncDecl(t *testing.T) {
-	ts := []*models.Token{
-		{
-			Type:  tokens.Function,
-			Value: "fn",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "hi",
-		},
-		{
-			Type:  tokens.LeftParenthesis,
-			Value: "(",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "username",
-		},
-		{
-			Type:  tokens.RightParenthesis,
-			Value: ")",
-		},
-		{
-			Type:  tokens.LeftBrace,
-			Value: "{",
-		},
-		{
-			Type:  tokens.Let,
-			Value: "let",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "a",
-		},
-		{
-			Type:  tokens.Assign,
-			Value: "=",
-		},
-		{
-			Type:  tokens.Number,
-			Value: "25",
-		},
-		{
-			Type:  tokens.Semicolon,
-			Value: ";",
-		},
-		{
-			Type:  tokens.RightBrace,
-			Value: "}",
-		},
-	}
+func Test_FnDecl(t *testing.T) {
+	nodes := build(t, `
+		fn main() {
+			return 0;
+		}
+	`)
 
-	b := NewBuilder()
-	nodes, err := b.Build(ts)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assert.Equal(t, 1, len(nodes), "must create on function node")
+	fn := nodes[0]
 
-	for _, node := range nodes {
-		fmt.Println(node.String())
-	}
+	assert.Equal(t, "main", fn.Content, "fn name must be main")
+	assert.Equal(t, 0, fn.Args, "args must be zero")
+	assert.Equal(t, 1, len(fn.Children), "must create return children")
 }
 
-func Test_AstDefineBlock(t *testing.T) {
-	ts := []*models.Token{
-		{
-			Type:  tokens.Define,
-			Value: "define",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "MyBlock",
-		},
-		{
-			Type:  tokens.LeftBrace,
-			Value: "{",
-		},
-		{
-			Type:  tokens.Let,
-			Value: "let",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "a",
-		},
-		{
-			Type:  tokens.Assign,
-			Value: "=",
-		},
-		{
-			Type:  tokens.Number,
-			Value: "25",
-			Map: map[string]any{
-				"isFloat": false,
-			},
-		},
-		{
-			Type:  tokens.Semicolon,
-			Value: ";",
-		},
-		{
-			Type:  tokens.Function,
-			Value: "fn",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "construct",
-		},
-		{
-			Type:  tokens.LeftParenthesis,
-			Value: "(",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "b",
-		},
-		{
-			Type:  tokens.RightParenthesis,
-			Value: ")",
-		},
-		{
-			Type:  tokens.LeftBrace,
-			Value: "{",
-		},
-		{
-			Type:  tokens.This,
-			Value: "this",
-		},
-		{
-			Type:  tokens.Dot,
-			Value: ".",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "a",
-		},
-		{
-			Type:  tokens.Assign,
-			Value: "=",
-		},
-		{
-			Type:  tokens.Identifier,
-			Value: "b",
-		},
-		{
-			Type:  tokens.Semicolon,
-			Value: ";",
-		},
-		{
-			Type:  tokens.RightBrace,
-			Value: "}",
-		},
-		{
-			Type:  tokens.RightBrace,
-			Value: "}",
-		},
-	}
+func Test_Define(t *testing.T) {
+	nodes := build(t, `
+		define MyApp {
+			let id = 0;
 
-	b := NewBuilder()
-	nodes, err := b.Build(ts)
-	if err != nil {
-		t.Fatal(err)
-	}
+			fn construct(id) {
+				this.id = id;
+			}
+		}
+	`)
 
-	for _, node := range nodes {
-		fmt.Println(node)
-	}
+	assert.Equal(t, 1, len(nodes), "must create one definition node")
+	definition := nodes[0]
+
+	assert.Equal(t, "MyApp", definition.Content, "definition name must be MyApp")
+	assert.Equal(t, 2, len(definition.Children), "definition must have 2 children")
+	variable := definition.Children[0]
+	fn := definition.Children[1]
+
+	assert.Equal(t, "id", variable.Content, "variable name should be id")
+	assert.Equal(t, 0, variable.Value, "variable value must be 0")
+
+	assert.Equal(t, "construct", fn.Content, "fn name should be construct")
+	assert.Equal(t, 1, len(fn.Args), "fn args must be 1")
+	assert.Equal(t, 1, len(fn.Children), "fn body children must be 1")
+	arg := fn.Args[0]
+	body := fn.Children[0]
+
+	assert.Equal(t, "id", arg.Content, "fn arg must be id")
+
+	assert.Equal(t, tokens.Assign, body.Type, "fn content must be an assignment")
+	assert.Equal(t, "this.id", body.Content, "fn must assign to this.id")
+	assert.Equal(t, "id", body.Value, "fn must assign id")
 }
